c82d66b64193573a0463e9e1c4249edea7f2c76c
raph <demmerleraphael@hotmail.com>
2021-01-26 22:07:59 +0100

    page recap exercices

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './progressbar.scss';
+
+const ProgressBar = () => (
+  <div className="progressbar-container">
+    <div className="progressbar">
+      <span className="span-progressbar">100%</span>
+    </div>
+  </div>
+
+);
+
+ProgressBar.propTypes = {
+
+};
+
+export default ProgressBar;
