6da652d36b02745a83dd8deb2505538724773603
raph <demmerleraphael@hotmail.com>
2021-02-01 10:13:19 +0100

    graphique dynamique avec les données du back

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -15,77 +15,81 @@ defaults.global.animation.onComplete = () => {
 // pour regler la animation du hover
 defaults.global.hover.animationDuration = 600;
 
-const Chart = ({ ExerciceName, AllPerfs }) => (
-  <div className="chart-container">
-    {console.log(AllPerfs)
-    }
-    <div className="chart">
-      <Line
-        data={{
-          labels:
-            AllPerfs.length > 1
-             ? AllPerfs.map((perf) => (perf.date.split(' ')[0])) : AllPerfs.date === null ? '' : AllPerfs.date.split(' ')[0],
+const Chart = ({ ExerciceName, AllPerfs }) => {
+  const checkPerf = AllPerfs.length === undefined;
 
-          datasets: [
-            {
-              label: 'KG',
-              // enleve le background de la ligne du graphique
-              fill: false,
-              data:
-               AllPerfs
-                && AllPerfs.length > 1
-                 ? AllPerfs.map((perf) => (perf.weight)) : AllPerfs.weight,
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-            },
-            {
-              label: 'repetition',
-              // enleve le background de la ligne du graphique
-              fill: false,
-              data: AllPerfs.length > 1 ? AllPerfs.map((perf) => (perf.repetition)) : AllPerfs.repetition,
-              borderColor: [
-                'rgba(0, 181, 204, 1)',
-              ],
-            },
-          ],
-        }}
-        options={{
+console.log(AllPerfs);
 
-          scales: {
-            yAxes: [
+  return (
+    <div className="chart-container">
+      <div className="chart">
+        <Line
+          data={{
+            labels: !checkPerf
+              && (AllPerfs.length > 1
+                ? AllPerfs.map((perf) => {
+                  const array = [];
+                  array.push(perf.date.split(' ')[0]);
+                  return array;
+                }) : AllPerfs[0].date.split(' ')[0]),
+            datasets: [
               {
-                // pour enlever les lines de l'axes y
-                gridLines: {
-                  display: false,
-                },
-                ticks: {
-                  beginAtZero: true,
-                },
+                label: 'KG',
+                // enleve le background de la ligne du graphique
+                fill: false,
+                data: !checkPerf && AllPerfs.map((perf) => (Number(perf.weight))),
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+              },
+              {
+                label: 'repetition',
+                // enleve le background de la ligne du graphique
+                fill: false,
+                data: !checkPerf && AllPerfs.map((perf) => (Number(perf.repetition))),
+                borderColor: [
+                  'rgba(0, 181, 204, 1)',
+                ],
               },
             ],
-            xAxes: [
-              {
-                // pour enlever les lines de l'axes X
-                gridLines: {
-                  display: false,
+          }}
+          options={{
+
+            scales: {
+              yAxes: [
+                {
+                  // pour enlever les lines de l'axes y
+                  gridLines: {
+                    display: false,
+                  },
+                  ticks: {
+                    beginAtZero: true,
+                  },
                 },
-                ticks: {
-                  fontSize: 10,
+              ],
+              xAxes: [
+                {
+                  // pour enlever les lines de l'axes X
+                  gridLines: {
+                    display: false,
+                  },
+                  ticks: {
+                    fontSize: 10,
+                  },
                 },
-              },
-            ],
-          },
-        }}
-      />
+              ],
+            },
+          }}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 // == Export
 export default Chart;
